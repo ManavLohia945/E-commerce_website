7e5473d3d247f3cbd812285a5084420fbc45ead9
Manav Lohia <manavlohia945@gmail.com>
2022-09-08 20:29:37 +0530

    Updated all files

---
+++ index.js
@@ -0,0 +1,9 @@
+const bar = document.getElementById('bar'); 
+const nav = document.getElementById('navbar');
+
+if(bar){ //this var 'bar' will store value as true if it is clicked that is if bar is clicked we want to do the foll functions
+    bar.addEventListener('click',() => { //we want to add and eventlistner when a click func is performed the arrow func will execute
+                                        //ie.our we add a class "active" to our navbar which will display that section
+        nav.classList.add('active');
+    })
+}
